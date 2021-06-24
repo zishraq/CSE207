c7357ef0f96a3bfee841619e84aa1a3396c9827d
zishraq <54944377+zishraq@users.noreply.github.com>
2021-06-25 02:17:13 +0600

    Update problem4.c
    Small changes

--- Lab7/problem4.c
+++ Lab7/problem4.c
@@ -8,106 +8,19 @@ typedef struct TreeNode {
     struct TreeNode *right;
 } TreeNode;
 
-typedef struct Queue {
-    int size;
-    int front;
-    int rear;
-    TreeNode **Q;
-} Queue;
-
-void queue_create(Queue *q, int size) {
-    q->size = size;
-    q->Q = (TreeNode **) malloc(q->size * sizeof(TreeNode *));
-    q->front = q->rear = -1;
-}
-
-void enqueue(Queue *q, TreeNode * x) {
-    if ((q->rear + 1) % q->size == q->front) {
-        printf("Queue is full\n");
-    } else {
-        q->rear = (q->rear + 1) % q->size;
-        q->Q[q->rear] = x;
-    }
-}
-
-TreeNode * dequeue(Queue *q) {
-    TreeNode *x = NULL;
-
-    if (q->rear == q->front) {
-        printf("Queue is empty\n");
-    } else {
-        q->front = (q->front + 1) %q->size;
-        x = q->Q[q->front];
-    }
-
-    return x;
-}
-
-int isQueueEmpty(Queue *q) {
-    return q->rear == q->front;
-}
-
-void tree_create(TreeNode **node, int arr[], int size) {
-    TreeNode *current, *temp;
-    int i = 0;
-    Queue *queue_tree = (Queue *) malloc(sizeof(Queue));
-
-    queue_create(queue_tree, 100);
-
-    (*node) = (TreeNode *) malloc(sizeof(TreeNode *));
-    (*node)->left = (*node)->right =  NULL;
-
-    (*node)->data = arr[i];
-
-    enqueue(queue_tree, (*node));
-
-    i++;
-    while (!isQueueEmpty(queue_tree) && i < size) {
-        current = dequeue(queue_tree);
-
-        temp = (TreeNode *) malloc(sizeof(TreeNode *));
-        temp->left = temp->right =  NULL;
-
+TreeNode * treeCreate(TreeNode *root, int *arr, int i, int size) {
+    if (i < size) {
+        TreeNode *temp = (TreeNode *) malloc(sizeof(TreeNode));
         temp->data = arr[i];
+        temp->left = temp->right = NULL;
 
-        current->left = temp;
+        root = temp;
 
-        enqueue(queue_tree, temp);
+        root->left = treeCreate(root->left, arr, 2 * i + 1, size);
 
-        i++;
-
-        if (i >= size) {
-            break;
-        }
-
-        temp = (TreeNode *) malloc(sizeof(TreeNode *));
-        temp->left = temp->right =  NULL;
-
-        temp->data = arr[i];
-
-        current->right = temp;
-
-        enqueue(queue_tree, temp);
-
-        i++;
+        root->right = treeCreate(root->right, arr, 2 * i + 2, size);
     }
-
-}
-
-void pre(TreeNode *root) {
-    if (root) {
-        printf("%d, ", root->data);
-        pre(root->left);
-        pre(root->right);
-    }
-}
-
-void inorder(TreeNode *root) {
-    if (root) {
-        inorder(root->left);
-        printf("%d, ", root->data);
-        inorder(root->right);
-    }
+    return root;
 }
 
 int isBST(TreeNode * root) {
@@ -127,19 +40,18 @@ int isBST(TreeNode * root) {
 }
 
 int main() {
-
     int arr[100];
     int size;
 
+    printf("Enter size: ");
     scanf("%d", &size);
 
     for (int i = 0; i < size; i++) {
+        printf("Enter element: ");
         scanf("%d", &arr[i]);
     }
 
-    TreeNode *root = NULL;
-
-    tree_create(&root, arr, size);
+    TreeNode *root = treeCreate(root, arr, 0, size);
 
     if (isBST(root)) {
         printf("true");
